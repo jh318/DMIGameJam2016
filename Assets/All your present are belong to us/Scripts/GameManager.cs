213e98582f472b062bfb00644b6793051578ce0c
jh318 <jhawley318@gmail.com>
2016-12-17 13:28:21 -0600

    added score tracking to gameManager. cleaned code

--- Assets/All your present are belong to us/Scripts/GameManager.cs
+++ Assets/All your present are belong to us/Scripts/GameManager.cs
@@ -1,40 +1,41 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour {
 
 	public static GameManager instance;
 
-	//_score can only be modified using the functions SubtractFromScore and AddToScore.
-	private int _score;
-	// Property that references the the private variable _score.
-	public int score{
-		get {return _score;}
-	}
-	// Use this for initialization
-	void Awake () {
-		if (instance == null){
-			instance = this;
-		}
-		else{
-		}		
+	private int _score; 	//_score can only be modified using the functions SubtractFromScore and AddToScore.
+	public int score{get {return _score;}} 	// Property that references the the private variable _score.
+	public Text scoreText;
+
+	void Awake () { // Use this for initialization
+		if (instance == null){instance = this;}
+		else{}
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+
 	}
 
-	 public void SubtractFromScore(int pointValue) {
+	public void SubtractFromScore(int pointValue) {
         instance._score -= pointValue;
+		instance.UpdateScore ();
         // instance.SetScoreText();
 		// AudioManager.PlayEffect("coinLost");
     }
 
     public void AddToScore(int pointValue) {
         instance._score += pointValue;
-  		//instance.SetScoreText();
+		instance.UpdateScore();
 		// AudioManager.PlayEffect("coinPickup");
     }
+
+	void UpdateScore ()
+	{
+		scoreText.text = "Score: " + _score;
+	}
 }
