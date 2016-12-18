94152eaf605066b9f60d89830e95f42e25dca8e8
MacMasterBlaster <mac.orchard@yahoo.com>
2016-12-17 19:05:48 -0600

    Added some things fixed the world

--- Assets/All your present are belong to us/Scripts/BasicEnemyMovement.cs
+++ Assets/All your present are belong to us/Scripts/BasicEnemyMovement.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Paraphernalia.Extensions;
 
 public class BasicEnemyMovement : MonoBehaviour {
 
@@ -11,20 +12,59 @@ public class BasicEnemyMovement : MonoBehaviour {
 	public float distanceLeft = 0.0f;
 	public float speed = 1.0f;
 
+	private CapsuleCollider collider;
+	private HealthController _healthCon;
+	private Rigidbody body;
+	private Animator animator;
+
+	private bool isDisabled = false;
 	private Vector3 positionLeft;
 	private Vector3 positionRight;
 	private Vector3 startingPos;
+
 	// Use this for initialization
+    void Awake() {
+		_healthCon = gameObject.GetComponent<HealthController>();
+		collider = gameObject.GetComponent<CapsuleCollider>();
+		animator = gameObject.GetComponent<Animator>();
+		body = gameObject.GetComponent<Rigidbody>();
+    }
+
 	void OnEnable () {
 		startingPos = gameObject.transform.position;
 		positionLeft = new Vector3((startingPos.x + distanceLeft), startingPos.y, startingPos.z);
 		positionRight = new Vector3((startingPos.x - distanceRight), startingPos.y, startingPos.z);
+    
+    //Listen for these events while enabled.
+    	_healthCon.onDeath += DisableMovementOnDeath;
+    	_healthCon.onDestruction += SpawnDeathParticles;
+    }
 
-	}
+    // Stop listening for these events when disabled.
+    void OnDisable() {
+    	_healthCon.onDeath -= DisableMovementOnDeath;
+    	_healthCon.onDestruction -= SpawnDeathParticles;
+    }
 	
 	// Update is called once per frame
 	void Update () {
 		//Move object between two positions at declared speed
+		if (isDisabled ==  false){
+			Move();
+		}
+	}
+
+	void DisableMovementOnDeath(){
+		// StartCoroutine("DisableMovementOnDeathCoroutine");
+		isDisabled = true;
+		animator.SetBool("isDisabled", true);
+		}
+
+	void SpawnDeathParticles(){
+		Spawner.Spawn("SnowmanDeath");
+	}
+
+	void Move(){
 		transform.position = Vector3.Lerp(
 			positionLeft, 
 			positionRight, 
@@ -32,6 +72,6 @@ public class BasicEnemyMovement : MonoBehaviour {
 				Time.time*speed, 
 				1.0f
 			)
-		);
+		);		
 	}
 }
